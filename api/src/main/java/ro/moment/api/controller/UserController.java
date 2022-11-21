d134ec9a3de7a77caff7678bf4185b96f19a0f41
Victor Gradinariu, INI-DOS-CSA (EXT) <victor.gradinariu@swisscom.com>
2022-11-21 16:40:04 +0200

    Added security to application

--- api/src/main/java/ro/moment/api/controller/UserController.java
+++ api/src/main/java/ro/moment/api/controller/UserController.java
@@ -1,19 +1,18 @@
 package ro.moment.api.controller;
+
 import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-import ro.moment.api.domain.Group;
+import ro.moment.api.domain.LoginDetails;
 import ro.moment.api.domain.User;
 import ro.moment.api.domain.dto.UserDto;
-import ro.moment.api.repository.UserRepository;
+import ro.moment.api.security.exceptions.RefreshTokenExpiredException;
+import ro.moment.api.security.tokens.Token;
 import ro.moment.api.service.UserService;
 
 import java.util.List;
-import java.util.Optional;
-import java.util.stream.Collectors;
-import java.util.stream.StreamSupport;
+
 
 @CrossOrigin
 @RestController
@@ -22,49 +21,58 @@ import java.util.stream.StreamSupport;
 public class UserController {
     private final UserService userService;
 
-    @RequestMapping( method=RequestMethod.GET)
-    public List<UserDto> getAll(){
+    @RequestMapping(method = RequestMethod.GET)
+    public List<UserDto> getAll() {
         System.out.println("Get all users ...");
         return userService.findAll();
     }
 
-    @RequestMapping(value = "/{id}", method = RequestMethod.GET)
-    public ResponseEntity<?> getById(@PathVariable String id){
-        System.out.println("Get by id "+id);
+    @PostMapping("/login")
+    public Token loginUser(@RequestBody LoginDetails details) throws Exception {
+        return userService.getTokensAtLogin(details.getUsername(), details.getPassword());
+    }
+
+    @PostMapping("/refresh")
+    public Token refreshAccessToken(@RequestBody String refreshToken) throws RefreshTokenExpiredException {
+        return userService.getTokensAtRefresh(refreshToken);
+    }
+
+    @GetMapping(value = "/{id}")
+    public ResponseEntity<?> getById(@PathVariable String id) {
+        System.out.println("Get by id " + id);
         UserDto user = userService.findById(Long.valueOf(id));
         if (user == null)
-            return new ResponseEntity<String>("User not found",HttpStatus.NOT_FOUND);
+            return new ResponseEntity<String>("User not found", HttpStatus.NOT_FOUND);
         else
-            return new ResponseEntity <UserDto>(user, HttpStatus.OK);
+            return new ResponseEntity<UserDto>(user, HttpStatus.OK);
     }
 
 
-    @RequestMapping(value = "/{username}", method = RequestMethod.GET)
-    public ResponseEntity<?> getByUsername(@PathVariable String username){
-        System.out.println("Get by username "+username);
+    @GetMapping(value = "/{username}")
+    public ResponseEntity<?> getByUsername(@PathVariable String username) {
+        System.out.println("Get by username " + username);
         UserDto user = userService.findByUsername(username);
-        if (user==null)
-            return new ResponseEntity<String>("User not found",HttpStatus.NOT_FOUND);
+        if (user == null)
+            return new ResponseEntity<String>("User not found", HttpStatus.NOT_FOUND);
         else
             return new ResponseEntity<UserDto>(user, HttpStatus.OK);
     }
 
     @RequestMapping(method = RequestMethod.POST)
-    public ResponseEntity<?> create(@RequestBody UserDto user){
+    public ResponseEntity<?> create(@RequestBody UserDto user) {
         userService.save(user);
         return new ResponseEntity<UserDto>(user, HttpStatus.CREATED);
-
     }
 
-    @RequestMapping(value="/{id}", method= RequestMethod.DELETE)
-    public ResponseEntity<?> delete(@PathVariable String id){
-        System.out.println("Deleting user ... "+id);
+    @DeleteMapping(value = "/{id}")
+    public ResponseEntity<?> delete(@PathVariable String id) {
+        System.out.println("Deleting user ... " + id);
         try {
             userService.deleteById(Long.valueOf(id));
             return new ResponseEntity<User>(HttpStatus.OK);
-        }catch (Exception ex){
+        } catch (Exception ex) {
             System.out.println("Ctrl Delete user exception");
-            return new ResponseEntity<String>(ex.getMessage(),HttpStatus.BAD_REQUEST);
+            return new ResponseEntity<String>(ex.getMessage(), HttpStatus.BAD_REQUEST);
         }
     }
 
