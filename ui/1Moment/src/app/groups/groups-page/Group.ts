6f23a71f2d32a9837bc2cbba3da31633bd48d0d1
Robert Giuga <grir2934@scs.ubbcluj.ro>
2023-01-17 21:21:44 +0200

    done

--- ui/1Moment/src/app/groups/groups-page/Group.ts
+++ ui/1Moment/src/app/groups/groups-page/Group.ts
@@ -1,8 +1,8 @@
-export interface Group{
-    id :number;
-    name: string;
-    // createdDate: string;
-    // updatedDate:string;
+export interface Group {
+  id: number;
+  name: string;
+  subscribed?: boolean;
+  // createdDate: string;
+  // updatedDate:string;
 
-  }
-  
+}
