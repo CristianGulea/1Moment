3ff7fe9c40ff45c47570a8ac7273d836f64d32bd
Robert Giuga <grir2934@scs.ubbcluj.ro>
2023-01-18 15:19:37 +0200

    search bar

--- ui/1Moment/src/app/groups/groups-page/groups-page.component.ts
+++ ui/1Moment/src/app/groups/groups-page/groups-page.component.ts
@@ -5,6 +5,7 @@ import {Group} from "./Group";
 import {Router} from "@angular/router";
 import {User} from "../../components/login/User";
 import {LoginService} from "../../components/login/login-service";
+import {debounceTime, distinctUntilChanged, Subject} from "rxjs";
 
 @Component({
   selector: 'app-groups-page',
@@ -14,24 +15,29 @@ import {LoginService} from "../../components/login/login-service";
 export class GroupsPageComponent implements OnInit {
 
   groups : Group[] = [];
+  groupsCopy: Group[] = [];
   isFetching: boolean = false;
   error: boolean = false;
   user:User={};
-  searchTerm = '';
+  searchTerm = new Subject<string>();
+  searchString: string;
 
-  constructor(private route: ActivatedRoute, private groupService: GroupService, private router: Router, private loginService: LoginService) { }
+  constructor(private route: ActivatedRoute, private groupService: GroupService, private router: Router, private loginService: LoginService) {
+    this.searchTerm.pipe(
+      debounceTime(300),
+      distinctUntilChanged())
+      .subscribe(value => {
+        this.onSearchGroup(value);
+      });
+  }
 
   ngOnInit(): void {
     this.isFetching=true;
     this.loginService.user.subscribe(value => this.user=value);
-    this.groupService.getGroups().subscribe(value => {this.groups= value; this.isFetching=false}, () =>{ this.error= true; this.isFetching=false});
+    this.groupService.getGroups().subscribe(value => {this.groups= value; this.groupsCopy=value; this.isFetching=false}, () =>{ this.error= true; this.isFetching=false});
     if(this.error){
       this.router.navigate(['/error']);
     }
-  }
-
-  onLike(arg0: any) {
-    throw new Error('Method not implemented.');
   }
 
   onLogout(){
@@ -42,14 +48,11 @@ export class GroupsPageComponent implements OnInit {
     this.router.navigate(['group/', group.id])
   }
 
-
-  onGoBack(){
-    throw new Error('Method not implemented.');
+  onSearchGroup(value:string){
+    this.groups= this.groupsCopy;
+    this.groups= this.groups.filter(value1 => value1.name.toLowerCase().includes(value.toLowerCase()));
   }
 
-  searchGroup(searchTerm: string) {
-    throw new Error('Method not implemented.');
-  }
 
   navigateToFeed() {
     this.router.navigate(["/feed"]);
